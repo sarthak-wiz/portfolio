4e6fb4a73e8a19263bf5b229937ce4ab30aaa10a
Sarthak Jain <sarthakj.study.cs@gmail.com>
2024-12-28 22:49:20 +0530

    first commit

---
+++ content.ts
@@ -0,0 +1,51 @@
+export const experiences = [
+  {
+    company: "Freelance",
+    role: "Software Developer",
+    range: "2024 - Current",
+    description:
+      "I work with clients to understand their business needs and provide technical solutions to help them achieve their goals.",
+    skills: ["Full Stack Development", "Devops", "AI Integration", "Finetuning", "RAG"],
+  },
+];
+
+export const Status = {
+  none: "none",
+  progress: "progress",
+  completed: "completed",
+} as const;
+
+export const photos = [
+  {
+    src: "/images/flower.jpg",
+    alt: "",
+  },
+  {
+    src: "/images/sunrays.jpg",
+    alt: "",
+  },
+  {
+    src: "/images/water.jpg",
+    alt: "",
+  },
+  {
+    src: "/images/tree.jpg",
+    alt: "",
+  },
+  {
+    src: "/images/boat.jpg",
+    alt: "",
+  }
+  
+];
+
+export const bucketList = [
+  {
+    item: "Run a marathon",
+    status: Status.none,
+  },
+];
+
+export const beliefs = [
+  "I believe in the power of technology to change the world.",
+];
